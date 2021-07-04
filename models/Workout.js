27f3b65615e51caf9c8ca15dabb7f509447b5f57
Diana Koeswanto <d.koeswanto@gmail.com>
2021-07-04 13:37:20 +1000

    First commit with models implemented

---
+++ models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+		type: Date,
+		default: Date.now
+	},
+	exercises: [
+		{
+			type: Schema.Types.ObjectId,
+			ref: "Exercise"
+		}
+	],
+	totalDuration: {
+		type: Number
+	},
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
